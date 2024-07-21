4445017f95f85282a49369d6e9b57f5ebbc54d45
salehkamalkamel <115103594+salehkamalkamel@users.noreply.github.com>
2024-07-21 14:50:21 +0300

    new project added

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,4 +1,4 @@
-import projects from "../data/about-data/projectsData";
+import { breifProjects } from "../data/about-data/projectsData";
 import skillsData from "../data/about-data/skillsData";
 import Footer from "../ui/Footer";
 import Header from "../ui/Header";
@@ -12,7 +12,7 @@ export default function Main() {
       <Header />
       <div id="content" className="pt-24 lg:w-1/2 lg:py-24">
         <About />
-        <ProjectsList projects={projects} />
+        <ProjectsList projects={breifProjects} />
         <SkillsList skills={skillsData} />
         <Footer />
       </div>
--- src/ui/TableBody.jsx
+++ src/ui/TableBody.jsx
@@ -1,10 +1,10 @@
-import projects from "../data/about-data/projectsData";
+import { fullProjects } from "../data/about-data/projectsData";
 import TableRow from "./TableRow";
 
 export default function TableBody() {
   return (
     <tbody>
-      {projects.map((project) => {
+      {fullProjects.map((project) => {
         return <TableRow row={project} key={project.title} />;
       })}
     </tbody>
