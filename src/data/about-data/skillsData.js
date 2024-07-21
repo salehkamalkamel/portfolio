bb0abb80cd5d89f3a4d5cd92736bbb8fce67f1d6
salehkamalkamel <115103594+salehkamalkamel@users.noreply.github.com>
2024-07-21 11:28:10 +0300

    Updated skills and tools sections to emphasize expertise

--- src/data/about-data/skillsData.js
+++ src/data/about-data/skillsData.js
@@ -1,56 +1,84 @@
 // skillsData.js
 import { FaReact, FaHtml5, FaCss3Alt, FaJsSquare } from "react-icons/fa";
-import { SiTailwindcss, SiStyledcomponents, SiSupabase } from "react-icons/si";
+import {
+  SiTailwindcss,
+  SiStyledcomponents,
+  SiSupabase,
+  SiRedux,
+  SiReactquery,
+  SiReactrouter,
+} from "react-icons/si";
 
 const skillsData = [
   {
     title: "HTML",
-    info: "A markup language for creating web pages.",
+    info: "Strong command of HTML, including advanced usage of tags, attributes, and SEO best practices.",
     link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
     tools: ["Tags", "Attributes", "SEO"],
     icon: FaHtml5,
   },
   {
     title: "CSS",
-    info: "A stylesheet language used to describe the presentation of a document written in HTML or XML.",
+    info: "Expertise in CSS, with a focus on Flexbox, Grid, and creating advanced animations for enhanced user experiences.",
     link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     tools: ["Flexbox", "Grid", "Animations"],
     icon: FaCss3Alt,
   },
   {
     title: "JavaScript",
-    info: "A programming language that is one of the core technologies of the World Wide Web.",
+    info: "Proficient in JavaScript, including ES6 features, DOM manipulation, and event handling for dynamic web applications.",
     link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     tools: ["ES6", "DOM", "Event Handling"],
     icon: FaJsSquare,
   },
   {
     title: "React",
-    info: "A JavaScript library for building user interfaces.",
+    info: "Strong understanding of React basics and performance optimization, including Hooks, Context API, and JSX.",
     link: "https://reactjs.org/",
     tools: ["Hooks", "Context API", "JSX"],
     icon: FaReact,
   },
   {
     title: "Tailwind CSS",
-    info: "A utility-first CSS framework for rapidly building custom designs.",
+    info: "Skilled in Tailwind CSS for rapidly building responsive and customized designs using utility classes.",
     link: "https://tailwindcss.com/",
     tools: ["Utility Classes", "Responsive Design", "Customization"],
     icon: SiTailwindcss,
   },
   {
     title: "Styled Components",
-    info: "Visual primitives for the component age, styled-components utilizes tagged template literals to style your apps.",
+    info: "Proficient in Styled Components, leveraging CSS-in-JS for theming and utilizing the Styled API for component-based styling.",
     link: "https://styled-components.com/",
     tools: ["CSS-in-JS", "Theming", "Styled API"],
     icon: SiStyledcomponents,
   },
   {
     title: "Supabase",
-    info: "An open source Firebase alternative.",
+    info: "Experience with Supabase for handling database management, authentication, and storage in web applications.",
     link: "https://supabase.io/",
     tools: ["Database", "Authentication", "Storage"],
     icon: SiSupabase,
+  },
+  {
+    title: "Redux",
+    info: "Expert in state management with Redux, including the use of the Redux Toolkit for efficient state management and asynchronous logic.",
+    link: "https://redux.js.org/",
+    tools: ["createSlice", "Thunk", "Middleware"],
+    icon: SiRedux,
+  },
+  {
+    title: "React Query",
+    info: "Proficient in React Query for data fetching, caching, and synchronization in React applications.",
+    link: "https://react-query.tanstack.com/",
+    tools: ["Data Fetching", "Caching", "Synchronization"],
+    icon: SiReactquery,
+  },
+  {
+    title: "React Router",
+    info: "Skilled in React Router for managing navigation and dynamic routing in single-page applications.",
+    link: "https://reactrouter.com/",
+    tools: ["Route", "Link", "useHistory"],
+    icon: SiReactrouter,
   },
 ];
 
